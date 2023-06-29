8e302b836199e2c8090ede3aeb537606f795c8bd
luongtuandung <luongtuandung181209@gmail.com>
2023-06-29 07:50:11 +0700

    commited

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('<int:id>/', views.post),
+    path('', views.index, name='blog'),
+    path('<int:id>/', views.post, name='post'),
 ]
